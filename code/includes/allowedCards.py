7d2839778c73f3408908e0d498a33348d4252d10
tame-lx-tech <5521011+tame-lx-tech@users.noreply.github.com>
2020-04-15 16:37:15 +0100

    add code and alter allowedCards

---
+++ code/includes/allowedCards.py
@@ -0,0 +1,5 @@
+#################################################
+## Path: code/includes/allowedCards.py         ##
+## Description: read file, sort into array etc ##
+## Date: 2020-04-15                            ##
+#################################################
