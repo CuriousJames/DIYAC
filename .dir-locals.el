8d3bc9e549736c28366e19faab456dd0d1c543f8
CuriousJames <18385102+CuriousJames@users.noreply.github.com>
2020-06-02 17:02:43 +0100

    Accidental commit of local system file

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;; The 'nil' configuration applies to all modes.
-    ((nil . ((indent-tabs-mode . t)
-            (tab-width . 1)))
-
-(add-hook 'python-mode-hook 'guess-style-guess-tabs-mode)	
-(add-hook 'python-mode-hook (lambda ()	
-	(guess-style-guess-tab-width)))
-	
