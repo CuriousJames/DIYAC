4e21bef5e9b7c54997cc365e6d337b0fc022114f
CuriousJames <18385102+CuriousJames@users.noreply.github.com>
2020-06-14 15:26:05 +0100

    Deleting unused folder structure

--- code/includes/allowedCards.py
+++
@@ -1,5 +0,0 @@
-#################################################
-## Path: code/includes/allowedCards.py         ##
-## Description: read file, sort into array etc ##
-## Date: 2020-04-15                            ##
-#################################################
